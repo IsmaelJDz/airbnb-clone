5268a24ddf02bf7b3e53d006933767dbae367e9b
Ismael R <ismael_br7@hotmail.com>
2023-04-25 15:00:33 -0600

    [AIRBNB: Page] Error page and auth middleware

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,10 +1,66 @@
 'use client';
-import useSearchModal from '@/app/hooks/useSearchModal';
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useSearchParams } from 'next/navigation';
 import { BiSearch } from 'react-icons/bi';
+
+import useCountries from '@/app/hooks/useCountries';
+import useSearchModal from '@/app/hooks/useSearchModal';
+import { differenceInDays } from 'date-fns';
 
 const Search = () => {
   const searchModal = useSearchModal();
+
+  const params = useSearchParams();
+  const { getByValue } = useCountries();
+
+  const locationValue = params?.get('locationValue');
+  const startDate = params?.get('startDate');
+  const endDate = params?.get('endDate');
+  const guestsCount = params?.get('guestCount');
+
+  const locationLabel = useMemo(() => {
+    if (locationValue)
+      return getByValue(locationValue as string)?.label;
+
+    return 'Anywhere';
+  }, [getByValue, locationValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} days`;
+
+      // const days =
+      //   (end.getTime() - start.getTime()) / (1000 * 3600 * 24);
+      // return `${days} days`;
+    }
+
+    return 'Any week';
+  }, [startDate, endDate]);
+
+  const guestsLabel = useMemo(() => {
+    if (guestsCount) {
+      return `${guestsCount} guests`;
+    }
+
+    return 'Add guests';
+  }, [guestsCount]);
+
+  // const guestLabel = useMemo(() => {
+  //   if (guestCount) {
+  //     return `${guestCount} Guests`;
+  //   }
+
+  //   return 'Add Guests';
+  // }, [guestCount]);
 
   return (
     <div
@@ -21,12 +77,14 @@ const Search = () => {
     cursor-pointer
   '>
       <div className='flex flex-row items-center justify-between'>
-        <div className='px-6 text-sm front-semibold'>Anywhere</div>
+        <div className='px-6 text-sm front-semibold'>
+          {locationLabel}
+        </div>
         <div className='hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center'>
-          Any week
+          {durationLabel}
         </div>
         <div className='flex flex-row items-center gap-3 pl-6 pr-2 text-sm text-gray-600'>
-          <div className='hidden sm:block'>Add Guests</div>
+          <div className='hidden sm:block'> {guestsLabel} </div>
           <div className='p-2 text-white rounded-full bg-rose-500'>
             <BiSearch size={18} />
           </div>
